a3ac62652e8054825e39a8f56ffe3d0f7c6260c9
Arnav Dixit <arnavdixit127@gmail.com>
2019-06-11 17:43:33 +0530

    Initial Commit

---
+++ func.h
@@ -0,0 +1,75 @@
+#include <termios.h>
+#include <cstdio>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+static struct termios old, newa;
+
+void initTermios(int echo)
+{
+    tcgetattr(0, &old);
+    newa = old;
+    newa.c_lflag &= ~ICANON;
+    newa.c_lflag &= echo ? ECHO : ~ECHO;
+    tcsetattr(0, TCSANOW, &newa);
+}
+
+void resetTermios(void)
+{
+    tcsetattr(0, TCSANOW, &old);
+}
+
+char getch_(int echo)
+{
+    char ch;
+    initTermios(echo);
+    ch = getchar();
+    resetTermios();
+    return ch;
+}
+
+char getch(void)
+{
+    return getch_(0);
+}
+
+char getche(void)
+{
+    return getch_(1);
+}
+
+int getmax_x()
+{
+    int lines = 24;
+    #ifdef TIOCGSIZE
+        struct ttysize ts;
+        ioctl(STDIN_FILENO, TIOCGSIZE, &ts);
+
+        lines = ts.ts_lines;
+    #elif defined(TIOCGWINSZ)
+        struct winsize ts;
+        ioctl ( STDIN_FILENO, TIOCGWINSZ, &ts);
+
+        lines = ts.ws_row;
+    #endif /* TIOCGSIZE */
+        return lines;
+}
+
+int getmax_y()
+{
+    int cols = 80;
+
+    #ifdef TIOCGSIZE
+        struct ttysize ts;
+        ioctl(STDIN_FILENO, TIOCGSIZE, &ts);
+        cols = ts.ts_cols;
+
+    #elif defined(TIOCGWINSZ)
+        struct winsize ts;
+        ioctl ( STDIN_FILENO, TIOCGWINSZ, &ts);
+        cols = ts.ws_col;
+
+    #endif /* TIOCGSIZE */
+    return cols;
+}
+
---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "menu.cpp"
+using namespace std;
+
+void add()
+{
+    cout<<"Add\n";
+}
+void sub()
+{
+    cout<<"Sub\n";
+}
+int main()
+{
+    cout<<"\nHello!\n";
+    menu s1;
+    s1.add("Multiply", 76, "Hellu Multiply");
+    s1.add("L", 412);
+    s1.add("Addition");
+    s1.add("Arnav Dixit 123", 3, "AAAA");
+    s1.add("Multiply", 76, "Hellu Multiply");
+    s1.add("L", 412);
+    s1.add("Addition");
+    s1.add("Arnav Dixit 123", 3, "AAAA");
+    s1.add("Subtraction");
+    s1.add("Subtraction");
+    int x=s1.display();
+    cout << endl<<endl<<x;
+    getch();
+    return 0;
+}
